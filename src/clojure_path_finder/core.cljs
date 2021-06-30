f1123ebeff49b09f9b6992e83199c8c8ec828d0b
moreover22 <amore@fi.uba.ar>
2021-06-30 08:13:58 -0300

    TDA grafo + intento de dfs

--- src/clojure_path_finder/core.cljs
+++ src/clojure_path_finder/core.cljs
@@ -20,7 +20,7 @@
 (defn home-page []
   [:div
    [h/header-component]
-   [m/map-component s/state]
+  ;;  [m/map-component s/state]
    ])
 
 ;; Drawing library
@@ -30,27 +30,46 @@
   (js/smooth))
 
 (def graph-view
-  @s/newstate)
+  {:model (:data s/graph) :size 80 :scale 0.7})
 
 
 (defn graph-view-get-vertices [graph]
-  (keys graph))
+  (keys (:model graph)))
+
+(defn point-in-circle? [x y c_x c_y r]
+  (let [dx (- x c_x)
+        dy (- y c_y)
+        dx2 (* dx dx)
+        dy2 (* dy dy)
+        d2 (+ dx2 dy2)]
+    (< d2 (* r r)))
+)
+
 
 (defn graph-view-draw-vertices [graph]
-  (doseq [node (graph-view-get-vertices graph)] 
-    (js/ellipse (* (:x node) 80) (* (:y node) 80) 60 60)
-  ))
+  (doseq [node (graph-view-get-vertices graph)]
+    (let [x (* (:x node) (:size graph))
+          y (* (:y node) (:size graph))
+          size (* (:size graph) (:scale graph))]
+      (if (point-in-circle? js/mouseX js/mouseY x y (/ size 2))
+        (js/strokeWeight 3)
+        (js/strokeWeight 1))
+      (js/ellipse x y size size))
+    )
+  )
 
 (defn graph-view-draw-edges [graph]
-  (doseq [[node adjacents] graph]
+  (doseq [[node adjacents] (:model graph)]
     (doseq [adjacent adjacents]
-      (js/line (* (:x node) 80) (* (:y node) 80) (* (:x adjacent) 80) (* (:y adjacent) 80)))))
+      (js/line (* (:x node) (:size graph)) 
+               (* (:y node) (:size graph)) 
+               (* (:x adjacent) (:size graph)) 
+               (* (:y adjacent) (:size graph))))))
 
 (defn draw []
   (js/background 51)
-  (js/strokeWeight 4)
+  (js/strokeWeight 2)
   (graph-view-draw-edges graph-view)
-  (js/strokeWeight 1)
   (graph-view-draw-vertices graph-view)
 )
 
@@ -62,7 +81,6 @@
 (set! (.-draw js/window) draw)
 
 (defn mount-root []
-  (println "testtest")
   (d/render [home-page] (.getElementById js/document "app")))
 
 (defn ^:export init! []
--- src/clojure_path_finder/state.cljs
+++ src/clojure_path_finder/state.cljs
@@ -1,4 +1,4 @@
-(ns clojure-path-finder.state 
+(ns clojure-path-finder.state
   (:require [reagent.core :as r]))
 
 (def state (r/atom [{:id 1 :x 1 :y 1 :adjacents [2 5]}
@@ -11,11 +11,78 @@
                     {:id 8 :x 4 :y 2 :adjacents [4 7]}]))
 
 ;; TODO: Enacapsular el nodo {x:... y:...}
-(def newstate (r/atom {{:x 1 :y 1} [{:x 2 :y 1} {:x 1 :y 2}]
-                       {:x 2 :y 1} [{:x 2 :y 1} {:x 3 :y 1} {:x 2 :y 2}]
-                       {:x 3 :y 1} [{:x 2 :y 1} {:x 4 :y 1} {:x 3 :y 2}]
-                       {:x 4 :y 1} [{:x 3 :y 1} {:x 4 :y 2}]
-                       {:x 1 :y 2} [{:x 2 :y 1} {:x 2 :y 2}]
-                       {:x 2 :y 2} [{:x 1 :y 2} {:x 2 :y 1} {:x 3 :y 2}]
-                       {:x 3 :y 2} [{:x 3 :y 1} {:x 1 :y 2} {:x 4 :y 2}]
-                       {:x 4 :y 2} [{:x 4 :y 1} {:x 3 :y 2}]}))
+;; (def newstate {{:x 1 :y 1} [{:x 2 :y 1} {:x 1 :y 2}]
+;;                 {:x 2 :y 1} [{:x 2 :y 1} {:x 3 :y 1} {:x 2 :y 2}]
+;;                 {:x 3 :y 1} [{:x 2 :y 1} {:x 4 :y 1} {:x 3 :y 2}]
+;;                 {:x 4 :y 1} [{:x 3 :y 1} {:x 4 :y 2}]
+;;                 {:x 1 :y 2} [{:x 2 :y 1} {:x 2 :y 2}]
+;;                 {:x 2 :y 2} [{:x 1 :y 2} {:x 2 :y 1} {:x 3 :y 2}]
+;;                 {:x 3 :y 2} [{:x 3 :y 1} {:x 1 :y 2} {:x 4 :y 2}]
+;;                 {:x 4 :y 2} [{:x 4 :y 1} {:x 3 :y 2}]})
+
+(defrecord Graph [data])
+
+(defn create-graph
+  ([] (Graph. {}))
+  ([data] (Graph. data)))
+
+(defn add-vertex [self vertex]
+  (if (contains? (:data self) vertex)
+    self
+    (create-graph (assoc (:data self) vertex []))))
+
+(defn get-adjacents [self vertex]
+  ((:data self) vertex))
+
+(defn add-adjacent [self vertex adjacent]
+  (let [old-adjacent (get-adjacents self vertex)
+        new-adjacent (conj old-adjacent adjacent)
+        new-data (assoc (:data self) vertex new-adjacent)]
+    (create-graph new-data)))
+
+(defn add-edge [self u v]
+  (let [one-way-graph (add-adjacent self u v)]
+    (add-adjacent one-way-graph v u)))
+
+(def edgeless-graph
+  (reduce add-vertex
+          (create-graph)
+          [{:x 1 :y 1}
+           {:x 2 :y 1}
+           {:x 3 :y 1}
+           {:x 4 :y 1}
+           {:x 1 :y 2}
+           {:x 2 :y 2}
+           {:x 3 :y 2}
+           {:x 4 :y 2}]))
+
+(def edges [[{:x 1 :y 1} {:x 2 :y 1}]
+            [{:x 1 :y 1} {:x 1 :y 2}]
+            [{:x 2 :y 1} {:x 1 :y 2}]
+            [{:x 2 :y 1} {:x 3 :y 1}]
+            [{:x 2 :y 1} {:x 2 :y 2}]
+            [{:x 3 :y 1} {:x 4 :y 1}]
+            [{:x 3 :y 1} {:x 3 :y 2}]
+            [{:x 4 :y 1} {:x 4 :y 2}]
+            [{:x 1 :y 2} {:x 2 :y 2}]
+            [{:x 2 :y 2} {:x 3 :y 2}]
+            [{:x 3 :y 2} {:x 4 :y 2}]])
+
+(def graph (reduce (fn [g [u v]] (add-edge g u v))
+                   edgeless-graph edges))
+
+(js/console.log (str (:data graph)))
+
+(defn dfs
+  ([graph source destination] (dfs graph source destination #{} {}))
+  ([graph source destination visited path]
+
+  (let [new-visited (conj visited source)]
+   (if (or (= source destination) (contains? visited source))
+     new-visited
+     (reduce (fn  [adjacents v]
+               (js/console.log (str adjacents))
+               (dfs graph v destination adjacents (assoc path v source))) new-visited (get-adjacents graph source))))))
+
+
+;; (js/console.log (str (dfs graph {:x 3 :y 1} {:x 4 :y 2})))
