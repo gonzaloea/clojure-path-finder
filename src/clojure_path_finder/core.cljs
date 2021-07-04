c36a3d4fc9135252fa42ed8181d8d20db3f73e41
Gonzalo Alvarez <gonza2703@gmail.com>
2021-07-03 23:23:28 -0300

    Dibujo de grafo en base a parametros. Formato de grilla en el dibujo. Manejo de estado en los casilleros. Cambio de visibilidad en base a click.

--- src/clojure_path_finder/core.cljs
+++ src/clojure_path_finder/core.cljs
@@ -3,8 +3,7 @@
    [reagent.dom :as d]
    [clojure-path-finder.components.header :as h]
    [clojure-path-finder.components.map :as m]
-   [clojure-path-finder.state :as s]
-   ))
+   [clojure-path-finder.state :as s]))
 
 ;; -------------------------
 ;; State
@@ -26,60 +25,76 @@
 ;; Drawing library
 
 (defn setup []
-  (js/createCanvas 640 480)
+  (js/createCanvas js/windowWidth js/windowHeight)
   (js/smooth))
 
 (def graph-view
-  {:model (:data s/graph) :size 80 :scale 0.7})
+  (atom {:model s/graph  :size 30 :scale 0.7}))
 
 
-(defn graph-view-get-vertices [graph]
-  (keys (:model graph)))
+(defn graph-view-get-vertices []
+  (keys (:data (:model @graph-view))))
 
-(defn point-in-circle? [x y c_x c_y r]
+(defn mouse-in-rect? [x y c_x c_y r]
   (let [dx (- x c_x)
         dy (- y c_y)
         dx2 (* dx dx)
         dy2 (* dy dy)
         d2 (+ dx2 dy2)]
-    (< d2 (* r r)))
-)
+    (< d2 (* r r))))
 
 
-(defn graph-view-draw-vertices [graph]
-  (doseq [node (graph-view-get-vertices graph)]
-    (let [x (* (:x node) (:size graph))
-          y (* (:y node) (:size graph))
-          size (* (:size graph) (:scale graph))]
-      (if (point-in-circle? js/mouseX js/mouseY x y (/ size 2))
+(defn graph-view-draw-vertices []
+  (doseq [node (graph-view-get-vertices)]
+    (let [x (* (:x node) (:size @graph-view))
+          y (* (:y node) (:size @graph-view))
+          size (* (:size @graph-view) (:scale @graph-view))]
+      ;;Si el puntero esta en el casillero, muestro el borde grueso, sino normal.
+      (if (mouse-in-rect? js/mouseX js/mouseY x y (/ size 2))
         (js/strokeWeight 3)
         (js/strokeWeight 1))
-      (js/ellipse x y size size))
-    )
-  )
+      ;;Si el puntero esta en el casillero y el mouseClick esta activo, pinto el fondo.
+      (if  (not (:visible node))
+        (js/fill "#000000")
+        (js/fill "#ffffff"))
+      ;;Dibujo el rectangulo del casillero
+      (js/rect (- x (/ size 2)) (- y (/ size 2)) size size))))
 
-(defn graph-view-draw-edges [graph]
-  (doseq [[node adjacents] (:model graph)]
+(defn graph-view-draw-edges []
+  (doseq [[node adjacents] (:data (:model @graph-view))]
     (doseq [adjacent adjacents]
-      (js/line (* (:x node) (:size graph)) 
-               (* (:y node) (:size graph)) 
-               (* (:x adjacent) (:size graph)) 
-               (* (:y adjacent) (:size graph))))))
+      (js/line (* (:x node) (:size @graph-view))
+               (* (:y node) (:size @graph-view))
+               (* (:x adjacent) (:size @graph-view))
+               (* (:y adjacent) (:size @graph-view))))))
 
 (defn draw []
-  (js/background 51)
+  (js/background "#ffffff")
   (js/strokeWeight 2)
-  (graph-view-draw-edges graph-view)
-  (graph-view-draw-vertices graph-view)
-)
+  (graph-view-draw-edges)
+  (graph-view-draw-vertices))
+
+(defn mouse-clicked []
+  (js/console.log "clicked")
+  (doseq [node (graph-view-get-vertices)]
+    (let [x (* (:x node) (:size @graph-view))
+          y (* (:y node) (:size @graph-view))
+          size (* (:size @graph-view) (:scale @graph-view))]
+      (if (mouse-in-rect? js/mouseX js/mouseY x y (/ size 2))
+        [(js/console.log (str "Previous: " @graph-view))
+         (swap! graph-view assoc :model (s/set-visibility (:model @graph-view) node (not (:visible node))))
+         (js/console.log (str "Post: "@graph-view))]))))
 
 ;; -------------------------
 ;; Initialize app
 
 
+
 (set! (.-setup js/window) setup)
 (set! (.-draw js/window) draw)
+(set! (.-mouseClicked js/window) mouse-clicked)
 
+(js/console.log (str @graph-view))
 (defn mount-root []
   (d/render [home-page] (.getElementById js/document "app")))
 
--- src/clojure_path_finder/state.cljs
+++ src/clojure_path_finder/state.cljs
@@ -1,14 +1,14 @@
 (ns clojure-path-finder.state
   (:require [reagent.core :as r]))
 
-(def state (r/atom [{:id 1 :x 1 :y 1 :adjacents [2 5]}
-                    {:id 2 :x 2 :y 1 :adjacents [1 3 6]}
-                    {:id 3 :x 3 :y 1 :adjacents [2 4 7]}
-                    {:id 4 :x 4 :y 1 :adjacents [3 8]}
-                    {:id 5 :x 1 :y 2 :adjacents [1 6]}
-                    {:id 6 :x 2 :y 2 :adjacents [5 2 7]}
-                    {:id 7 :x 3 :y 2 :adjacents [3 5 8]}
-                    {:id 8 :x 4 :y 2 :adjacents [4 7]}]))
+;; (def state (r/atom [{:id 1 :x 1 :y 1 :adjacents [2 5]}
+;;                     {:id 2 :x 2 :y 1 :adjacents [1 3 6]}
+;;                     {:id 3 :x 3 :y 1 :adjacents [2 4 7]}
+;;                     {:id 4 :x 4 :y 1 :adjacents [3 8]}
+;;                     {:id 5 :x 1 :y 2 :adjacents [1 6]}
+;;                     {:id 6 :x 2 :y 2 :adjacents [5 2 7]}
+;;                     {:id 7 :x 3 :y 2 :adjacents [3 5 8]}
+;;                     {:id 8 :x 4 :y 2 :adjacents [4 7]}]))
 
 ;; TODO: Enacapsular el nodo {x:... y:...}
 ;; (def newstate {{:x 1 :y 1} [{:x 2 :y 1} {:x 1 :y 2}]
@@ -32,12 +32,24 @@
     (create-graph (assoc (:data self) vertex []))))
 
 (defn get-adjacents [self vertex]
-  ((:data self) vertex))
+  (vertex (:data self)))
+
 
 (defn add-adjacent [self vertex adjacent]
   (let [old-adjacent (get-adjacents self vertex)
         new-adjacent (conj old-adjacent adjacent)
         new-data (assoc (:data self) vertex new-adjacent)]
+    (create-graph new-data)))
+
+(defn new-vertex
+  ([x y] (into {} (map (fn [[k v]] [k v]) {:x x :y y :visible true})))
+  ([x y visible] (into {} (map (fn [[k v]] [k v]) {:x x :y y :visible visible}))))
+
+
+(defn set-visibility [self vertex visibility]
+  (let [new-v (new-vertex (:x vertex) (:y vertex) visibility)
+        adjacent (get-adjacents self vertex)
+        new-data (assoc (dissoc (:data self) vertex) new-v adjacent)]
     (create-graph new-data)))
 
 (defn add-edge [self u v]
@@ -47,42 +59,41 @@
 (def edgeless-graph
   (reduce add-vertex
           (create-graph)
-          [{:x 1 :y 1}
-           {:x 2 :y 1}
-           {:x 3 :y 1}
-           {:x 4 :y 1}
-           {:x 1 :y 2}
-           {:x 2 :y 2}
-           {:x 3 :y 2}
-           {:x 4 :y 2}]))
+          (into []
+                (for [i (range 1 30) j (range 1 20)]
+                  (new-vertex i j true)))))
 
-(def edges [[{:x 1 :y 1} {:x 2 :y 1}]
-            [{:x 1 :y 1} {:x 1 :y 2}]
-            [{:x 2 :y 1} {:x 1 :y 2}]
-            [{:x 2 :y 1} {:x 3 :y 1}]
-            [{:x 2 :y 1} {:x 2 :y 2}]
-            [{:x 3 :y 1} {:x 4 :y 1}]
-            [{:x 3 :y 1} {:x 3 :y 2}]
-            [{:x 4 :y 1} {:x 4 :y 2}]
-            [{:x 1 :y 2} {:x 2 :y 2}]
-            [{:x 2 :y 2} {:x 3 :y 2}]
-            [{:x 3 :y 2} {:x 4 :y 2}]])
+
+
+(def edges
+  [[{:x 1 :y 1} {:x 2 :y 1}]
+   [{:x 1 :y 1} {:x 1 :y 2}]
+   [{:x 2 :y 1} {:x 1 :y 2}]
+   [{:x 2 :y 1} {:x 3 :y 1}]
+   [{:x 2 :y 1} {:x 2 :y 2}]
+   [{:x 3 :y 1} {:x 4 :y 1}]
+   [{:x 3 :y 1} {:x 3 :y 2}]
+   [{:x 4 :y 1} {:x 4 :y 2}]
+   [{:x 1 :y 2} {:x 2 :y 2}]
+   [{:x 2 :y 2} {:x 3 :y 2}]
+   [{:x 3 :y 2} {:x 4 :y 2}]])
 
 (def graph (reduce (fn [g [u v]] (add-edge g u v))
                    edgeless-graph edges))
 
-(js/console.log (str (:data graph)))
+
+
 
 (defn dfs
   ([graph source destination] (dfs graph source destination #{} {}))
   ([graph source destination visited path]
 
-  (let [new-visited (conj visited source)]
-   (if (or (= source destination) (contains? visited source))
-     new-visited
-     (reduce (fn  [adjacents v]
-               (js/console.log (str adjacents))
-               (dfs graph v destination adjacents (assoc path v source))) new-visited (get-adjacents graph source))))))
+   (let [new-visited (conj visited source)]
+     (if (or (= source destination) (contains? visited source))
+       new-visited
+       (reduce (fn  [adjacents v]
+                 (js/console.log (str adjacents))
+                 (dfs graph v destination adjacents (assoc path v source))) new-visited (get-adjacents graph source))))))
 
 
-;; (js/console.log (str (dfs graph {:x 3 :y 1} {:x 4 :y 2})))
+
